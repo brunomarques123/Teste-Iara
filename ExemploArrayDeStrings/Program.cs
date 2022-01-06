5817a83eeac691222c5b92f42d0e3cadd8764728
Bruno Marques <b_marques85@live.com>
2022-01-06 20:24:37 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploArrayDeStrings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExemploArrayDeStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palavras = { "abracadabra", "allottee", "assessee", "kelless", "keenness"};
+
+            // Length quantidade registros do array
+            for (int i = 0; i < palavras.Length; i++) 
+            {
+                Console.WriteLine(RemoverDuplicados(palavras[i]));
+            }
+        }
+
+        // recebe que está correndo no for
+        static string RemoverDuplicados(string nome) 
+        {
+            // Armazena as letras, somente um caractere
+            char antes = ' ';
+
+            // armazena o resultado
+            string resultado = "";
+
+            // Percorre cada caractere do nome
+            foreach (char letra in nome)
+            {
+                if (antes != letra)
+                {
+                    antes = letra;
+                    resultado += letra;
+                }
+            }
+
+            return resultado;
+        }
+    }
+
+}
